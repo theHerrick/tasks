aed669d3e7a74e91a229981f24322980127ccc52
Simon Herrick <simon@herrick.io>
2023-07-11 16:10:28 +0100

    docker working. Started frontend.

---
+++ docker/frontend/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Tasks</h1>
+        <p>
+         <ul>
+          <li>Task #1</li>
+          <li>Task #2</li>
+          <li>Task #3</li>
+          </ul>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
