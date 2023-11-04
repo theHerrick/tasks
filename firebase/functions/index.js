3d434e59d296fc499145f14176c24b2b44ee5715
Simon Herrick <simon@herrick.io>
2023-11-04 07:27:42 +0000

    CRUD complete

---
+++ firebase/functions/index.js
@@ -0,0 +1,21 @@
+//index.js
+
+const functions = require('firebase-functions');
+const app = require('express')();
+
+const {
+    getAllTodos,
+    postOneTodo,
+    deleteTodo,
+    editTodo
+} = require('./APIs/todos')
+
+app.get('/todos', getAllTodos);
+
+app.post('/todo', postOneTodo);
+
+app.delete('/todo/:todoId', deleteTodo);
+
+app.put('/todo/:todoId', editTodo);
+
+exports.api = functions.https.onRequest(app);
