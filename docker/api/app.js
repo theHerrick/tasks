25a818e82dab1766388e1597036930a2e61a34fe
Simon Herrick <simon@herrick.io>
2023-11-04 14:05:59 +0000

    API complete

--- docker/api/app.js
+++ docker/api/app.js
@@ -1,48 +1,17 @@
 const express = require('express');
-const mysql = require('mysql');
-const cors = require('cors'); // Import the cors package
+const cors = require('cors');
 const app = express();
 
-// Enable CORS for all routes
+// Cors
 app.use(cors());
 
-// MySQL database connection configuration
-const dbConfig = {
-    host: 'localhost',
-    user: 'root',
-    password: 'adminpassword',
-    database: 'todo',
-    port: 3306, // Replace with the desired port number
-  };
+// Require the todoRoutes
+const todos = require('./api/todos'); // Adjust the path as needed
 
-// Create a MySQL connection
-const connection = mysql.createConnection(dbConfig);
+// Start using the todo routes
+app.use('/', todos);
 
-// Connect to the MySQL database
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err);
-    return;
-  }
-  console.log('Connected to MySQL database');
-});
-
-// Define a route to fetch data from the database
-app.get('/api/data', (req, res) => {
-  const query = 'SELECT * FROM todo';
-
-  connection.query(query, (err, rows) => {
-    if (err) {
-      console.error('Error executing MySQL query:', err);
-      res.status(500).json({ error: 'Internal Server Error' });
-      return;
-    }
-
-    res.json(rows);
-  });
-});
-
-// Start the Express server
+// Start Express
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
---
+++ docker/api/util/database.js
@@ -0,0 +1,10 @@
+const dbConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: 'adminpassword',
+    database: 'todo',
+    port: 3306,
+  };
+  
+  module.exports = dbConfig;
+  
